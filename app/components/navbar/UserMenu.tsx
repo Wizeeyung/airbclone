62d52ceceb49110fb5112a6b9cc5745628c6abce
wizeeyung <ogunbanwosamson@gmail.com>
2024-09-07 17:35:14 +0100

    navbar modification

---
+++ app/components/navbar/UserMenu.tsx
@@ -0,0 +1,17 @@
+
+
+const UserMenu = () => {
+  return (
+    <div className="relative">
+      <div className="flex items-center gap-3">
+        <div onClick={()=>{}} className="hidden md:block text-sm font-semibold py-3 px-4 border-[1px] rounded-full hover:bg-neutral-100 transition cursor-pointer">
+          Airbnb your home
+        </div>
+        
+      </div>
+      
+    </div>
+  )
+}
+
+export default UserMenu
