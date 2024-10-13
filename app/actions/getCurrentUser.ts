6447f96aab02d305d9b9eac5cc01195ef8b591e1
wizeeyung <ogunbanwosamson@gmail.com>
2024-10-13 04:55:55 +0100

    auth fixes

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "@/utils/authOptions";
 
 
 //we are using this function in the layout page cause its a server component by default
